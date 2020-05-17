53d309e2c1c7688a65f8a561fedc6b289dfa5d0d
Matheus Martins da Silva <cin_masilva@uolinc.com>
2020-05-17 02:33:33 -0300

    refactor: add singleton inject for User Repository

--- src/utils/container/index.ts
+++ src/utils/container/index.ts
@@ -1,8 +1,5 @@
-import 'reflect-metadata';
 import { container } from 'tsyringe';
 
 import UserRepository from '@domain/user/repository/UserRepository';
 
-container.register('UserRepository', {
-  useClass: UserRepository,
-});
+container.registerSingleton('UserRepository', UserRepository);
